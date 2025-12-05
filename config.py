1a0d0cd1b7a174ebdfc715c6a28dc03408c63cb0
Manal Mulla <141561252+manalmulla@users.noreply.github.com>
2025-12-06 00:15:03 +0530

    Replace NASA_MAP_KEY with placeholder value
    Updated NASA_MAP_KEY and NASA_FIRMS_CSV_URL to use a placeholder.

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
 import os
-NASA_MAP_KEY = os.getenv("NASA_MAP_KEY", "c0fd730a795a9fe1036b36f7526b4473")
+NASA_MAP_KEY = os.getenv("NASA_MAP_KEY", "YOUR_MAP_KEY")
 
-NASA_FIRMS_CSV_URL = f"https://firms.modaps.eosdis.nasa.gov/api/area/csv/c0fd730a795a9fe1036b36f7526b4473/VIIRS_SNPP_NRT/world/1"
+NASA_FIRMS_CSV_URL = f"https://firms.modaps.eosdis.nasa.gov/api/area/csv/YOUR_MAP_KEY/VIIRS_SNPP_NRT/world/1"
 
 # Confidence thresholds:
 # >=80 -> high -> red
